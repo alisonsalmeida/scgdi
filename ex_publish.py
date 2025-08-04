84b25231e8f6fd3311a1bea389eccd87e634b8ee
alison.almeida <alisonalmeida.py@gmail.com>
2025-08-04 10:22:04 -0400

    refactor: create task sensor to publish data

--- ex_publish.py
+++ ex_publish.py
@@ -1,17 +1,56 @@
 from ex_mqtt import MQTTChannel
 import asyncio
+import random
+
+
+async def task_sensor_electrical(channel):
+	while True:
+		voltage = random.uniform(220.0, 240.0)
+		current = random.uniform(5.0, 7.0)
+		power = voltage * current
+
+		channel.client.publish(
+			'scgdi/sensor/electrical', 
+			{'Voltage': voltage, 'Current': current, 'Power': power}
+		)
+
+		await asyncio.sleep(1)
+
+
+async def task_sensor_vibration(channel):
+	while True:
+		accell_x = random.uniform(-0.05, 0.05)
+		accell_y = random.uniform(-0.05, 0.05)
+		accell_z = random.uniform(-0.05, 0.05)
+
+		channel.client.publish(
+			'scgdi/sensor/vibration', 
+			{'Accell_X': accell_x, 'Accell_Y': accell_y, 'Accell_Z': accell_z}
+		)
+
+		await asyncio.sleep(1)
+
+async def task_sensor_environment(channel):
+	while True:
+		temperature = random.uniform(22.0, 24.0)
+		humidity = random.uniform(41.0, 44.0)
+
+		channel.client.publish(
+			'scgdi/sensor/environment', 
+			{'Temperature': temperature, 'Humidity': humidity}
+		)
+
+		await asyncio.sleep(1)
 
 
 async def main():
 	channel = MQTTChannel()
 	await channel.init()
+	asyncio.create_task(task_sensor_electrical(channel))
+	asyncio.create_task(task_sensor_environment(channel))
+	asyncio.create_task(task_sensor_vibration(channel))	
 
-	count = 0
-	while True:
-		channel.client.publish('topico-aleatorio', count)
-		count += 1
-		
-		await asyncio.sleep(1)
+	await asyncio.Event().wait()
 
 
 if __name__ == '__main__':
